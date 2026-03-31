198c8df708699a8c696bb5e7def470c0d2c8edcc
Raed Khalil <202311019@students.asu.edu.jo>
2026-03-31 11:56:23 +0300

    Refactor VertexSearchConfig class formatting

--- lib/core/constants/vertex_search_config.dart
+++ lib/core/constants/vertex_search_config.dart
@@ -4,8 +4,3 @@ class VertexSearchConfig {
   static const String apiKey = '<your_api_key>';
 }
 
-
-// project id: project-31722430-64b6-45ea-a4c 
-// data store id: visioncareer_1773921415472
-// apiKey: AIzaSyBwB3zoWWGNRKnD4EbPicNJEGxmgqbbbk0 (through google could website "My first project" "API key 1")
-//gemini api key: AIzaSyDcQFwIsR_pkwL-09SfmNc639ZcMGRJr5Q
