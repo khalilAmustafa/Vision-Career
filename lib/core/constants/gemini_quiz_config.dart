e914aa1827c5460bbdb1c75b5ccaa3d788b8672a
Raed Khalil <202311019@students.asu.edu.jo>
2026-04-13 20:16:17 +0300

    Update gemini_quiz_config.dart

--- lib/core/constants/gemini_quiz_config.dart
+++ lib/core/constants/gemini_quiz_config.dart
@@ -2,7 +2,7 @@ class GeminiQuizConfig {
   const GeminiQuizConfig._();
 
   // Move this out of the client before production release.
-  static const String apiKey = 'AIzaSyBwneguG4W5UHZUK4I_4HnwEzRRkktnoEo';
+  static const String apiKey = 'Your api key';
   static const String model = 'gemini-2.5-flash';
 
   static String get endpoint =>
