45b76423d584e3f212eaaf81f2451d7b124f20b2
Raed Khalil <202311019@students.asu.edu.jo>
2026-04-13 19:45:36 +0300

    Delete lib directory

--- lib/core/constants/vertex_search_config.dart
+++
@@ -1,6 +0,0 @@
-class VertexSearchConfig {
-  static const String projectId = 'project-31722430-64b6-45ea-a4c';
-  static const String dataStoreId = 'visioncareer_1773921415472';
-  static const String apiKey = '<your_api_key>';
-}
-
