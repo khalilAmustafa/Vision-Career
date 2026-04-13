fc720887caa9673ec94a2703ed54a141bb94c89c
Raed Khalil <202311019@students.asu.edu.jo>
2026-04-13 20:16:50 +0300

    Update vertex_search_config.dart

--- lib/core/constants/vertex_search_config.dart
+++ lib/core/constants/vertex_search_config.dart
@@ -4,8 +4,3 @@ class VertexSearchConfig {
   static const String apiKey = 'your api key';
 }
 
-
-// project id: project-31722430-64b6-45ea-a4c 
-// data store id: visioncareer_1773921415472
-// apiKey: AIzaSyAixI_k7BSmgOVU2fNDQpeT-09agyNomXU (through google could website "My first project" "API key 1")
-//gemini api key: AIzaSyA1bwtIveNhzX5fkafESoqvzSeXsF28dTM
