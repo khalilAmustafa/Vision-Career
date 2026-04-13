f3b28f8b61030fcd6c213915691b967d8d5a7dcd
Raed Khalil <202311019@students.asu.edu.jo>
2026-04-13 20:16:40 +0300

    Change apiKey to placeholder in VertexSearchConfig
    Updated the apiKey to a placeholder value for security.

--- lib/core/constants/vertex_search_config.dart
+++ lib/core/constants/vertex_search_config.dart
@@ -1,7 +1,7 @@
 class VertexSearchConfig {
   static const String projectId = 'project-31722430-64b6-45ea-a4c';
   static const String dataStoreId = 'visioncareer_1773921415472';
-  static const String apiKey = 'AIzaSyAixI_k7BSmgOVU2fNDQpeT-09agyNomXU';
+  static const String apiKey = 'your api key';
 }
 
 
